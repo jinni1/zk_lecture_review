fa8199475dd8be80458ff802ab7c85e8abccb02b
sejin <tp028@naver.com>
2025-08-02 07:03:29 +0900

    수강생 등록, 조회

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -1,3 +1,48 @@
+import { AppDataSource } from "../db.js";
+
+const CommitmentRepo = () => AppDataSource.getRepository("StudentCommitment");
+
+
 export const registerStudent = async (req, res) => {
-    
+    const { lectureId, commitment } = req.body;
+
+    try {
+        const repo = CommitmentRepo();
+
+        const exists = await repo.find({
+            where: {commitment}
+        });
+
+        if (exists.length != 0 ) {
+            console.log("exists:", exists);
+            return res.status(400).json({ message: "이미 등록된 수강생입니다."})
+        }
+
+        const student = repo.create({ lectureId, commitment });
+        await repo.save(student);
+
+        return res.status(200).json({ message: "수강생 등록 완료"});
+    } catch (err) {
+        console.log("수강생 등록 에러: ", err);
+        return res.status(500).json({ err: "수강생 등록 중 에러가 발생했습니다." })
+    }
+} 
+
+export const getStudentList = async (req, res) => {
+    const lectureId = Number(req.params.lectureId);
+    try {
+        const repo = CommitmentRepo();
+        const commitments = await repo.find({
+            where: {lectureId}
+        });
+
+        const result = commitments.map( c => c.commitment);
+        if (result.length==0) {
+            return res.status(200).json({ message: "등록된 수강생이 존재하지 않습니다"});
+        }
+        return res.status(200).json({ message: "수강생 조회 성공", result});
+    } catch (err) {
+        console.log("수강생 조회 에러: ", err);
+        return res.status(500).json({ err: "수강생 조회 중 에러가 발생했습니다." })
+    }
 }
--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: process.env.DB_HOST,
-    port: process.env.PORT,
+    port: process.env.DB_PORT,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
